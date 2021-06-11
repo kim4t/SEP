f995d2094365b5c590b9e046cfc6499c3ea279ee
kim4t <taetaeho.kim@gmail.com>
2021-06-11 11:32:54 -0400

    Revision

--- C# 02/Exercise4/Program.cs
+++ C# 02/Exercise4/Program.cs
@@ -12,9 +12,34 @@ namespace Exercise4
 
         static void Print(int[,] arr)
         {
-            for(int i=0;i<arr.GetLength(0);i++)
-                for(int j=0;j< arr.GetLength(1); j++)
-                    Console.Write($"{ arr[i,j]} ");
+
+            int i, a = 0, b = 0;
+            int row = arr.GetLength(0);
+            int col = arr.GetLength(1);
+
+            while (a < row && b < col)
+            {
+                for (i = b; i < col; ++i)
+                    Console.Write(arr[a, i] + " ");
+                a++;
+                for (i = a; i < row; ++i)                
+                    Console.Write(arr[i, col - 1] + " ");               
+                col--;
+
+                if (a < row)
+                {
+                    for (i = col - 1; i >= b; --i)                    
+                        Console.Write(arr[row - 1, i] + " ");                    
+                    row--;
+                }
+                if (b < col)
+                {
+                    for (i = row - 1; i >= a; --i)                    
+                        Console.Write(arr[i, b] + " ");                    
+                    b++;
+                }
+            }
         }
+    
     }
 }
