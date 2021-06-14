e5cac7a280ff21f999aa0cdd501cfe4e09e73169
kim4t <taetaeho.kim@gmail.com>
2021-06-14 19:38:11 -0400

    C# 05

---
+++ C# 05/Exercise3/MainClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class MainClass
+    {
+        static  void f(ref string s)
+              {
+            s += " world";
+        }
+        [STAThread]
+        static void Main (string[] args)
+       {
+            String s = "Hello";
+            f(ref s);
+            Console.WriteLine(s);
+        }
+    }
+
+}
